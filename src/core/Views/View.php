ab4e55f2b99431cf80fa383459df123b69fecc4b
Valentino Pesce <valentino@iltuobrand.it>
2019-06-03 23:52:40 +0200

    add instance of frontend translations

--- src/core/Views/View.php
+++ src/core/Views/View.php
@@ -14,14 +14,41 @@
 
 namespace Instant\Core\Views;
 
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+
 class View 
 {
 	public function show($file, $data = null)
 	{
+		if (isset($_SESSION['Active'])) {
+			$users = json_decode(file_get_contents(realpath('src/config/users.json')), true);
+			$usernames = [];
+			foreach ($users as $user) { array_push($usernames,$user['Username']); }
+			$currentUser = array_search($_SESSION['Username'],$usernames);
+			$userLanguage = $users[$currentUser]['Language'];
+	
+			if (isset($userLanguage)) {
+				$t = new Translator($userLanguage);
+				$t->addLoader('array', new ArrayLoader());
+				if (file_exists('src/translations/'.$userLanguage.'.php')) {
+					include 'src/translations/'.$userLanguage.'.php';
+				} else {
+					include 'src/translations/'.LANGUAGE.'.php';
+				} 
+			} 
+		} elseif (file_exists('src/translations/'.LANGUAGE.'.php')) {
+			include 'src/translations/'.LANGUAGE.'.php';
+		} else {
+			echo "Make sure that the config.php file is present in the config folder and that the language code is entered.";
+			exit;
+		}
+		
 		if (is_array($data))
 		{
 			extract($data);
 		}
+
 		include 'src/views/'.$file;
 	}
 }
