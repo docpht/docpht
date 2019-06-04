dfb81a9a0df3508227468457ae1a0c6efb2d357b
creecros <creecros@gmail.com>
2019-06-04 20:00:40 +0000

    ModifySectionForm.php recoding

--- src/forms/ModifySectionForm.php
+++ src/forms/ModifySectionForm.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 /**
  * This file is part of the DocPHT project.
@@ -11,170 +11,134 @@
  * file that was distributed with this source code.
  */
 
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../lib/functions.php';
-require __DIR__.'/../lib/Model.php';
+namespace DocPHT\Form;
 
 use Nette\Forms\Form;
+use Nette\Utils\Html;
+use DocPHT\Lib\DocBuilder;
+use DocPHT\Model\PageModel;
+use DocPHT\Core\Translator\T;
 
-$db = new DocData;
-$docBuilder = new DocBuilder();
-
-$aPath = $_SESSION['update_path'];
-$id = $db->getId($aPath);
-
-$jsonArray = $db->getPageData($id);
-
-if(isset($_GET['id'])) {
-    $rowIndex = $_GET['id'];
-}
-
-function makeBootstrap4(Form $form)
+class ModifySectionForm extends MakeupForm
 {
-	$renderer = $form->getRenderer();
-	$renderer->wrappers['controls']['container'] = null;
-	$renderer->wrappers['pair']['container'] = 'div class="form-group"';
-	$renderer->wrappers['pair']['.error'] = 'has-danger';
-	$renderer->wrappers['control']['container'] = 'div class=col';
-	$renderer->wrappers['label']['container'] = 'div class="col col-form-label font-weight-bold"';
-	$renderer->wrappers['control']['description'] = 'span class=form-text';
-	$renderer->wrappers['control']['errorcontainer'] = 'span class=form-error';
-
-	foreach ($form->getControls() as $control) {
-		$type = $control->getOption('type');
-		if ($type === 'button') {
-			$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-secondary btn-block' : 'btn btn-primary');
-			$usedPrimary = true;
-
-		} elseif (in_array($type, ['text', 'textarea', 'select'], true)) {
-			$control->getControlPrototype()->addClass('form-control selectpicker');
-
-		} elseif ($type === 'file') {
-			$control->getControlPrototype()->addClass('form-control-file');
-
-		} elseif (in_array($type, ['checkbox', 'radio'], true)) {
-			if ($control instanceof Nette\Forms\Controls\Checkbox) {
-				$control->getLabelPrototype()->addClass('form-check-label');
-			} else {
-				$control->getItemLabelPrototype()->addClass('form-check-label');
-			}
-			$control->getControlPrototype()->addClass('form-check-input');
-			$control->getSeparatorPrototype()->setName('div')->addClass('form-check');
-		}
+    private $pageModel;
+    private $doc;
+    
+	public function __construct()
+	{
+		$this->pageModel = new PageModel();
+		$this->doc = new DocBuilder();
 	}
-}
-
-$languages = json_decode(file_get_contents(realpath('data/doc-pht/code-array.json')), true);
-
-$form = new Form;
-$form->onRender[] = 'makeBootstrap4';
-
-$form->addGroup(T::trans('Create new notes'));
-
-$options = [
-    'title' => T::trans('Add title'),
-    'description' => T::trans('Add description'),
-	'pathAdd'  => T::trans('Add path'),
-	'codeInline' => T::trans('Add code inline'),
-	'codeFile' => T::trans('Add code from file'),
-	'blockquote' => T::trans('Add blockquote'),
-	'image' => T::trans('Add image from file'),
-	'imageURL' => T::trans('Add image from url'),
-	'markdown' => T::trans('Add markdown'),
-	'linkButton' => T::trans('Add link button')
-];
-
-if ($jsonArray[$rowIndex]['key'] != 'addButton') {
     
-    $form->addSelect('options', T::trans('Options:'), $options)
-    	->setPrompt(T::trans('Select an option'))
-    	->setHtmlAttribute('data-live-search','true')
-    	->setDefaultValue($jsonArray[$rowIndex]['key'])
-    	->setRequired(T::trans('Select an option'));
-    	
-    
-    $form->addSelect('language', T::trans('Language:'), $languages)
-    	->setPrompt(T::trans('Select an option'))
-    	->setHtmlAttribute('data-live-search','true')
-    	->setRequired(T::trans('Select an option'));
-    	
-    if ($jsonArray[$rowIndex]['key'] == 'codeInline' || $jsonArray[$rowIndex]['key'] == 'codeFile') {
-        $form['language']->setDefaultValue($jsonArray[$rowIndex]['v2']); 
-    } else {
-        $form['language']->setDefaultValue('Markup');
-    }
-    
-    $form->addUpload('file', 'File:')
-        ->setRequired(false)
-        ->addRule(Form::MIME_TYPE, 'File must be JPEG, PNG, GIF or Plain Text.', ['image/jpeg','image/gif','image/png','text/plain'])
-		->addRule(Form::MAX_FILE_SIZE, 'Maximum file size is 10 mb.', 10 * 1024 * 1024 /* size in MB */);
-    	
-    $form->addTextArea('option_content', T::trans('Option Content'))
-        ->setHtmlAttribute('data-parent', 'options')
-        ->setAttribute('data-autoresize'); 
-    	
-    if ($jsonArray[$rowIndex]['key'] == 'image') {
-        $form['option_content']->setDefaultValue($jsonArray[$rowIndex]['v2']); 
-    } else {
-        $form['option_content']->setDefaultValue($jsonArray[$rowIndex]['v1']);
-    }
-    
-    if ($jsonArray[$rowIndex]['key'] == 'imageURL' || $jsonArray[$rowIndex]['key'] == 'linkButton') { 
-        $name = $jsonArray[$rowIndex]['v2']; 
-    } else { 
-        $name = ''; 
-    }
-    
-    if ($jsonArray[$rowIndex]['key'] == 'linkButton') { 
-        ($jsonArray[$rowIndex]['v3']) ? $trg = true : $trg = false;
-    } else { 
-        $trg = false; 
-    }
+    public function create()
+    {
+
+        $uPath = $_SESSION['update_path'];
+        $languages = $this->doc->listCodeLanguages();
+        $options = $this->doc->getOptions();
+
+        $form = new Form;
+        $form->onRender[] = [$this, 'bootstrap4'];
+
+        $page = $this->pageModel->getPageData($id);
+
+        if(isset($_GET['id'])) {
+            $rowIndex = $_GET['id'];
+        }
+
+        $form->addGroup(T::trans('Create new notes'));
         
-        $form->addTextArea('names', T::trans('Name'))
-            ->setHtmlAttribute('data-parent', 'options'.$index)
-            ->setAttribute('data-autoresize')
-        	->setDefaultValue($name);
+        if ($page[$rowIndex]['key'] != 'addButton') {
+            
+            $form->addSelect('options', T::trans('Options:'), $options)
+            	->setPrompt(T::trans('Select an option'))
+            	->setHtmlAttribute('data-live-search','true')
+            	->setDefaultValue($page[$rowIndex]['key'])
+            	->setRequired(T::trans('Select an option'));
+            	
+            
+            $form->addSelect('language', T::trans('Language:'), $languages)
+            	->setPrompt(T::trans('Select an option'))
+            	->setHtmlAttribute('data-live-search','true')
+            	->setRequired(T::trans('Select an option'));
+            
+            if ($page[$rowIndex]['key'] == 'codeInline' || $page[$rowIndex]['key'] == 'codeFile') {
+                $form['language']->setDefaultValue($page[$rowIndex]['v2']); 
+            } else {
+                $form['language']->setDefaultValue('Markup');
+            }
+            
+            $form->addUpload('file', 'File:')
+                ->setRequired(false)
+                ->addRule(Form::MIME_TYPE, 'File must be JPEG, PNG, GIF or Plain Text.', ['image/jpeg','image/gif','image/png','text/plain'])
+        		->addRule(Form::MAX_FILE_SIZE, 'Maximum file size is 10 mb.', 10 * 1024 * 1024 /* size in MB */);
+            	
+            $form->addTextArea('option_content', T::trans('Option Content'))
+                ->setHtmlAttribute('data-parent', 'options')
+                ->setAttribute('data-autoresize'); 
+            	
+            if ($page[$rowIndex]['key'] == 'image') {
+                $form['option_content']->setDefaultValue($page[$rowIndex]['v2']); 
+            } else {
+                $form['option_content']->setDefaultValue($page[$rowIndex]['v1']);
+            }
+            
+            if ($page[$rowIndex]['key'] == 'imageURL' || $page[$rowIndex]['key'] == 'linkButton') { 
+                $name = $page[$rowIndex]['v2']; 
+            } else { 
+                $name = ''; 
+            }
+            
+            if ($page[$rowIndex]['key'] == 'linkButton') { 
+                ($page[$rowIndex]['v3']) ? $trg = true : $trg = false;
+            } else { 
+                $trg = false; 
+            }
+                
+                $form->addTextArea('names', T::trans('Name'))
+                    ->setHtmlAttribute('data-parent', 'options'.$index)
+                    ->setAttribute('data-autoresize')
+                	->setDefaultValue($name);
+                	
+                $form->addCheckbox('trgs', T::trans('Open in New Window?'))
+                    ->setHtmlAttribute('data-parent', 'options')
+                    ->setAttribute('data-autoresize')
+                	->setDefaultValue($trg);
         	
-        $form->addCheckbox('trgs', T::trans('Open in New Window?'))
-            ->setHtmlAttribute('data-parent', 'options')
-            ->setAttribute('data-autoresize')
-        	->setDefaultValue($trg);
-	
-} 
+        } 
 
-$form->addProtection(T::trans('Security token has expired, please submit the form again'));
-
-$form->addSubmit('submit', T::trans('Modify'));
-
-$success = '';
-
-if ($form->isSuccess()) {
-    $values = $form->getValues();
-    
-    if ($jsonArray[$rowIndex]['key'] == 'image' || $jsonArray[$rowIndex]['key'] == 'codeFile') { unlink('data/' . $jsonArray[$rowIndex]['v1']); }
-    $docBuilder->removeOldFile($jsonArray[$rowIndex]['key'], $values['options'], 'data/' . $jsonArray[$rowIndex]['v1']);
-
-	if (!empty($values)) {
-	    
-        $file = $values['file'];
-        $file_path = $docBuilder->upload($file, $db->getPhpPath($id));
+        $form->addProtection(T::trans('Security token has expired, please submit the form again'));
         
-		$folder = substr(pathinfo($aPath, PATHINFO_DIRNAME ), 4);
-		$filename = pathinfo($aPath, PATHINFO_FILENAME );
-		
-	    if(isset($id)) {
-    	    $db->modifyPageData($id, $rowIndex, $docBuilder->valuesToArray($values, $file_path));
-    	    $docBuilder->buildPhpPage($id);
-            header('location:index.php?p='.$db->getFilename($id).'&f='.$db->getTopic($id));
-			exit;
-	    } else {
-	        echo '<p class="text-center text-success">'.T::trans("Sorry something didn't work!").'</p>'; 
-	    }
-	}   
+        $form->addSubmit('submit', T::trans('Modify'));
+        
+        $success = '';
+
+        if ($form->isSuccess()) {
+            $values = $form->getValues();
+            
+            if ($page[$rowIndex]['key'] == 'image' || $page[$rowIndex]['key'] == 'codeFile') { unlink('data/' . $page[$rowIndex]['v1']); }
+            $this->doc->removeOldFile($page[$rowIndex]['key'], $values['options'], 'data/' . $page[$rowIndex]['v1']);
+        
+        	if (!empty($values)) {
+        	    
+                $file = $values['file'];
+                $file_path = $this->doc->upload($file, $this->pageModel->getPhpPath($id));
+                
+        		$folder = substr(pathinfo($uPath, PATHINFO_DIRNAME ), 4);
+        		$filename = pathinfo($uPath, PATHINFO_FILENAME );
+        		
+        	    if(isset($id)) {
+            	    $this->pageModel->modifyPageData($id, $rowIndex, $this->doc->valuesToArray($values, $file_path));
+            	    $this->doc->buildPhpPage($id);
+                    header('Location:index.php?p='.$this->pageModel->getFilename($id).'&f='.$this->pageModel->getTopic($id));
+        			exit;
+        	    } else {
+    				$bad = T::trans('Sorry something didn\'t work!');
+    				header('Location:index.php?p='.$this->pageModel->getFilename($id).'&f='.$this->pageModel->getTopic($id));
+    				exit;
+        	    }
+        	}   
+        }
+        return $form;
+    }
 }
-
-
-echo '<div class="container-fluid"><div class="card"><div class="card-body">';
-echo $form;
-echo '</div></div></div>';
