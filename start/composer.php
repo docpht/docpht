a31cae080e3213ed3d180416e2e99fcf80e19770
Valentino Pesce <valentino@iltuobrand.it>
2019-06-11 15:14:05 +0200

    add require-dev and update command in the startup guides

--- start/composer.php
+++ start/composer.php
@@ -1,4 +1,4 @@
 <?php include 'start/partial/head.php'; ?>
     <h3><i class="fa fa-caret-right" aria-hidden="true"></i> Step 2</h3>
-    <p>Install composer dependencies, run: <code>composer install</code></p>
+    <p>Install composer dependencies, run: <code>composer install --no-dev --no-suggest</code></p>
 <?php include 'start/partial/footer.php'; ?>
