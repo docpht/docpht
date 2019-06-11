3db6a9bfb74ad163887c80b960749306b5bd35e6
Valentino Pesce <valentino@iltuobrand.it>
2019-06-11 15:45:15 +0200

    add footer

--- src/views/partial/footer.php
+++ src/views/partial/footer.php
@@ -1,3 +1,14 @@
+            
+            <footer class="footer">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="#" class="text-muted" target="_blank">DocPHT</a>. All rights reserved.</span>
+                            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-muted">Valentino Pesce <i class="fa fa-code-fork" aria-hidden="true"></i> Craig Crosby</span>
+                        </div>
+                    </div>    
+                </div>        
+            </footer>
 
         </div> <!-- end Page Content  -->
     </div> <!-- end wrapper -->
