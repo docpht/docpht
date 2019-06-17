62f7047e0c7b81cf0adbec67144ae0cddbd15364
creecros <creecros@gmail.com>
2019-06-17 21:23:53 +0000

    fix prev/next and sidepanel children

--- src/views/page/page.php
+++ src/views/page/page.php
@@ -46,7 +46,7 @@ if (!is_null($topics)) {
         if (!empty($topics)) {
             $allpages = array();
             foreach ($topics as $topic) {
-                $pages = $this->pageModel->getPublishedPagesByTopic($topic);
+                (isset($_SESSION['Active'])) ? $pages = $this->pageModel->getPagesByTopic($topic) : $pages = $this->pageModel->getPublishedPagesByTopic($topic);
                 if($pages !== false)$allpages = array_merge($allpages,$pages);
             }
         }
