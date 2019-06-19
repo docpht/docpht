32ca492a8392628db2cf33432b512345d880ff0b
creecros <creecros@gmail.com>
2019-06-19 18:29:16 +0000

    add card to settings panel

--- src/views/admin/settings.php
+++ src/views/admin/settings.php
@@ -143,6 +143,21 @@
                     </div>
                 </div>
 
+                <div class="col-md-4 grid-margin mb-4">
+                    <div class="card bg-docpht d-flex align-items-left">
+                        <a href="admin/backup" class="text-white">
+                            <div class="card-body shadow">
+                                <div class="d-flex flex-row align-items-left">
+                                        <i class="fa fa-database fa-3x" aria-hidden="true"></i>
+                                    <div class="ml-3">
+                                        <h6 class="text-white"><?= $t->trans('Backups'); ?></h6>
+                                        <p class="mt-2 text-white card-text"><small><?= $t->trans('Backups'); ?></small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
 
         </div>
