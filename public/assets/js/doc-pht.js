7087a2520575a70bb7d595d991d57231d9fc8179
Valentino Pesce <valentino@iltuobrand.it>
2019-06-22 11:13:28 +0200

    rename scroll indicator function

--- public/assets/js/doc-pht.js
+++ public/assets/js/doc-pht.js
@@ -90,9 +90,9 @@ $(document).ready(function () {
         }
     });
 
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function() {pageScrollIndicator()};
 
-    function myFunction() {
+    function pageScrollIndicator() {
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
         var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         var scrolled = (winScroll / height) * 100;
