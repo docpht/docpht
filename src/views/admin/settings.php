66b3ff63e2341c791e6a13d5a12bb514872af313
Valentino Pesce <valentino@iltuobrand.it>
2019-08-26 09:56:05 +0200

    add the card button for the last access list

--- src/views/admin/settings.php
+++ src/views/admin/settings.php
@@ -193,6 +193,24 @@
                     </div>
                 </div>
                 <?php endif ?>
+
+                <?php if (isset($_SESSION['Active']) && $admin == true): ?>
+                <div class="col-md-4 grid-margin mb-4">
+                    <div class="card bg-docpht d-flex align-items-left">
+                        <a href="admin/lastlogin" class="text-white">
+                            <div class="card-body shadow">
+                                <div class="d-flex flex-row align-items-left">
+                                        <i class="fa fa-sign-in fa-3x" aria-hidden="true"></i>
+                                    <div class="ml-3">
+                                        <h6 class="text-white"><?= $t->trans('Last login'); ?></h6>
+                                        <p class="mt-2 text-white card-text"><small><?= $t->trans('Log'); ?></small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <?php endif ?>
                 
             </div>
 
